d0de3c1b03ae83f5a4bfde0b350249929c03d001
Gergely Brautigam <gergely@arangodb.com>
2019-12-15 11:21:41 +0100

    WIP

--- intcode.go
+++ intcode.go
@@ -3,6 +3,7 @@ package intcode
 import (
 	"fmt"
 	"strconv"
+	"time"
 )
 
 const (
@@ -59,35 +60,38 @@ func (m *Machine) ProcessProgram() (out []int, done bool) {
 loop:
 	for {
 		opcode := m.Memory[m.Position]
-		op, modes := getOpCodeAndModes(opcode)
-		//fmt.Printf("Op: %d, modes: %+v\n", op, modes)
-		//fmt.Println(Memory)
-		//time.Sleep(1 * time.Second)
-		//fmt.Println("i, op: ", i, op)
+		codes := fmt.Sprintf("%05d", opcode)
+		fmt.Println("Codes: ", codes)
+		op, _ := strconv.Atoi(codes[3:])
+		modes := codes
+		fmt.Printf("Op: %d, modes: %+v\n", op, modes)
+		fmt.Println(m.Memory)
+		time.Sleep(1 * time.Second)
 		switch op {
 		case add:
-			args := m.getArguments(3, modes)
-			m.Memory[args[2]] = args[0] + args[1]
+			arg1, arg2 := m.getParameter(1, modes), m.getParameter(2, modes)
+			dest := m.getDestination(3, modes)
+			m.Memory[dest] = arg1 + arg2
 			m.Position += 4
 		case multi:
-			args := m.getArguments(3, modes)
-			m.Memory[args[2]] = args[0] * args[1]
+			//args := m.getArguments(3, modes)
+			//m.Memory[args[2]] = args[0] * args[1]
+			arg1, arg2 := m.getParameter(1, modes), m.getParameter(2, modes)
+			dest := m.getDestination(3, modes)
+			m.Memory[dest] = arg1 * arg2
 			m.Position += 4
 		case input:
-			//fmt.Println(m.Memory)
-			args := m.getArguments(1, modes)
+			dest := m.getDestination(3, modes)
 			//fmt.Printf("Modes: %+v; Position: %d; Op: %d Args: %+v; Value At pos: %d\n", modes, m.Position, op, args, m.Memory[args[0]])
 			if len(m.Input) > 0 {
 				var in int
 				in, m.Input = m.Input[0], m.Input[1:]
-				m.Memory[m.Memory[args[0]]] = in
+				m.Memory[dest] = in
+				//fmt.Println(m.Memory)
 			} else {
-				m.Memory[m.Memory[args[0]]] = m.Memory[args[0]]
+				m.Memory[m.Memory[dest]] = dest
 			}
-			//fmt.Println(m.Memory)
 			m.Position += 2
-			//fmt.Printf("In for %q is: %d\n", m.Name, m.Input)
-			//fmt.Println(args)
 		case output:
 			//var oout int
 			//if len(modes) > 0 {
@@ -104,50 +108,52 @@ loop:
 			//	oout = m.Memory[m.Memory[m.Position+1]]
 			//}
 			//fmt.Println(m.Memory)
-			args := m.getArguments(1, modes)
+			dest := m.getDestination(1, modes)
 			//fmt.Printf("args: %+v\n", args)
-			//fmt.Printf("Modes: %+v; Position: %d; Op: %d Args: %+v\n", modes, m.Position, op, args)
-			out = append(out, args[0])
-			//out = append(out, oout)
+			fmt.Printf("Modes: %+v; Position: %d; Op: %d\n", modes, m.Position, op)
+			//out = append(out, args[0])
+			out = append(out, dest)
 			//fmt.Printf("Out of %q is: %+v\n", m.Name, out)
 			m.Position += 2
 		case jmp:
-			args := m.getArguments(2, modes)
-			if args[0] != 0 {
-				m.Position = args[1]
+			arg1 := m.getParameter(1, modes)
+			if arg1 != 0 {
+				m.Position = arg1
 			} else {
 				m.Position += 3
 			}
 			//fmt.Printf("5 i: %d args: %+v\n", i, args)
 		case jmpf:
-			args := m.getArguments(2, modes)
-			if args[0] == 0 {
-				m.Position = args[1]
+			arg1 := m.getParameter(1, modes)
+			if arg1 == 0 {
+				m.Position = arg1
 			} else {
 				m.Position += 3
 			}
 			//fmt.Printf("6 i: %d args: %+v\n", i, args)
 		case less:
-			args := m.getArguments(3, modes)
-			if args[0] < args[1] {
-				m.Memory[args[2]] = 1
+			arg1, arg2 := m.getParameter(1, modes), m.getParameter(2, modes)
+			dest := m.getDestination(3, modes)
+			if arg1 < arg2 {
+				m.Memory[dest] = 1
 			} else {
-				m.Memory[args[2]] = 0
+				m.Memory[dest] = 0
 			}
 			//fmt.Printf("7 i: %d args: %+v\n", i, args)
 			m.Position += 4
 		case eq:
-			args := m.getArguments(3, modes)
-			if args[0] == args[1] {
-				m.Memory[args[2]] = 1
+			arg1, arg2 := m.getParameter(1, modes), m.getParameter(2, modes)
+			dest := m.getDestination(3, modes)
+			if arg1 == arg2 {
+				m.Memory[dest] = 1
 			} else {
-				m.Memory[args[2]] = 0
+				m.Memory[dest] = 0
 			}
 			//fmt.Printf("8 i: %d args: %+v\n", i, args)
 			m.Position += 4
 		case adj:
-			args := m.getArguments(1, modes)
-			m.RelativeBase += args[0]
+			arg1 := m.getParameter(1, modes)
+			m.RelativeBase += arg1
 			m.Position += 2
 		case 99:
 			break loop
@@ -159,49 +165,27 @@ loop:
 	return out, true
 }
 
-func (m *Machine) getArguments(num int, modes []int) (args []int) {
-	for p := 0; p < num; p++ {
-		var mode int
-		if p >= len(modes) {
-			mode = 0
-		} else {
-			mode = modes[p]
-		}
-		switch mode {
-		case position:
-			// Because parameters that an instruction writes to is always in position mode.
-			if p > 1 && p+1 == num {
-				args = append(args, m.Memory[m.Position+p+1])
-			} else {
-				args = append(args, m.Memory[m.Memory[m.Position+p+1]])
-			}
-		case immediate:
-			args = append(args, m.Memory[m.Position+p+1])
-		case relative:
-			if p > 1 && p+1 == num {
-				//fmt.Println("Relative Base: ", m.RelativeBase)
-				args = append(args, m.Memory[m.Position+p+1])
-			} else {
-				pos := m.RelativeBase + m.Memory[m.Position+p+1]
-				args = append(args, m.Memory[pos])
-			}
-		}
+func (m *Machine) getParameter(index int, mode string) (address int) {
+	switch mode[3-index] {
+	case '0':
+		address = m.Memory[m.Position+index]
+	case '1':
+		address = m.Position + index
+	case '2':
+		address = m.RelativeBase + m.Memory[m.Position+index]
 	}
 	return
 }
 
-func getOpCodeAndModes(opcode int) (o int, modes []int) {
-	sop := strconv.Itoa(opcode)
-	l := len(sop)
-	if len(sop) == 1 {
-		o, _ = strconv.Atoi(sop)
-		return o, nil
-	}
-	o, _ = strconv.Atoi(sop[l-2:])
-	smodes := sop[:l-2]
-	for i := len(smodes) - 1; i >= 0; i-- {
-		m, _ := strconv.Atoi(string(smodes[i]))
-		modes = append(modes, m)
+func (m *Machine) getDestination(index int, mode string) (dest int) {
+	fmt.Println("Mode 3-index: ", string(mode[3-index]))
+	switch mode[3-index] {
+	case '0', '1':
+		dest = m.Memory[m.Position+index]
+		fmt.Println("dest in 0,1: ", dest)
+	case '2':
+		dest = m.RelativeBase + m.Memory[m.Position+index]
+		fmt.Println("dest in 2: ", dest)
 	}
 	return
 }
